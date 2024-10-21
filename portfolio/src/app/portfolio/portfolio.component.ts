c26bf17645cd37f6a6ac62bdc2953a7bd20a6b1b
jemimakathar <katharolijemima@gmail.com>
2024-10-21 17:19:35 +0530

    uploaded file

---
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent {
+  constructor(public titleService:Title)
+  {
+    this.titleService.setTitle('jemima-portfolio')
+  }
+
+}
