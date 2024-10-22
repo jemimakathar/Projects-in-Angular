4a82763e9f6b43740491799803ba7d4f7e78a062
jemimakathar <katharolijemima@gmail.com>
2024-10-22 16:49:08 +0530

    uploaded file.ts

--- portfolio/src/app/project-card/project-card.component.ts
+++ portfolio/src/app/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-card.component.css']
 })
 export class ProjectCardComponent {
+  @Input() projectName:string=""; //input decorator use panna athu child
 
 }
